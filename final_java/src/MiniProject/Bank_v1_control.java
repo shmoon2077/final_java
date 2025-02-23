5ef403dd10df57e259e712f746015f95e3b2b85c
shmoon2077 <margincall2077@icloud.com>
2025-02-23 18:16:22 +0900

    https://youtu.be/_77mv856JOQ?si=eSJDoRyI_cDRs_ed
    첫 미니프로젝트 bank 완성. 주어진 코드만으로는 구현하기 어려운 부분이 있어서 equals 메서드를 사용해야 했다 (아직 안배웠는데...)
    아이디를 변수를 char로 설정하면 1개의 알파벳만 식별할 수 있는데, 그러면 불완전한 부분이 있어 String을 이용해야 했다.
    String 경우엔 ==나 !=로 비교할 수 없어서 equals를 부정하는 방식으로 반복문을 만들었다.

--- final_java/src/MiniProject/Bank_v1_control.java
+++ final_java/src/MiniProject/Bank_v1_control.java
@@ -6,26 +6,30 @@ public class Bank_v1_control {
 	public static void main (String [] args) {
 	//변수
 	String temp_id = null , id = null;
-	int num,pw = 0,temp_pw,bal = 0;
+	int num,pw = 0,temp_pw,bal = 0, depo = 0, with= 0;
+	char ch = ' ';
 	Scanner scan = new Scanner(System.in);
+	
 	
 	for (;;) { //무한 반복
 		System.out.print("\n\n========KBANK========\r\n"+"1.추가\r\n"+"2.조회\r\n"+"3.입금\r\n"
 	                     +"4.출금\r\n"+"5.삭제\r\n"+"9.종료\r\n"+"메뉴를 선택하세요 >>  ");
 		num = scan.nextInt();
+		scan.nextLine();
 		if (num==9) {System.out.println("종료합니다."); break;}
 		switch(num) {
 			case 1 : System.out.println("추가 기능입니다.");
 			
 					 System.out.println("아이디를 입력하세요");
-					 scan.nextLine();
 					 id = scan.nextLine();
 					 
 					 System.out.println("비밀번호를 입력하세요");
 					 pw = scan.nextInt();
+					 scan.nextLine();
 					 
 					 System.out.println("잔액을 입력하세요");
-					 bal = scan.nextInt();			 	
+					 bal = scan.nextInt();
+					 scan.nextLine();
 				// step3
 				// id _입력받기 first
 				// pw _입력받기 1111
@@ -34,38 +38,50 @@ public class Bank_v1_control {
 			case 2 : System.out.println("조회 기능입니다.");
 			
 				     System.out.println("아이디를 입력하세요.");
-				     scan.nextLine();
 				     temp_id = scan.nextLine();
-				     while (temp_id!=id) {
+				     while (!temp_id.equals(id)) {
 				     System.out.println("아이디를 확인하세요");
-				     scan.nextLine();
-				     temp_id = scan.nextLine();}
+				     temp_id = scan.nextLine();
+				     }
 				     
 				     System.out.println("비밀번호를 입력하세요.");
 				     temp_pw = scan.nextInt();
+				     scan.nextLine();
 				     while (temp_pw!=pw) {
 				     System.out.println("비밀번호를 확인하세요.");
 				     temp_pw = scan.nextInt();};
-				   
+				     scan.nextLine();
 				     System.out.println("잔액은 : "+bal);
-				  
-				  			
-			
 				break; //end case 2			
-			case 3 : System.out.println("입금 기능입니다.");
+
+			
+			case 3 : System.out.println("입금 기능입니다. 입금하실 금액을 입력해주세요.");
+			  		 depo = scan.nextInt();
+			  	     scan.nextLine();
+			  		 bal += depo;
+			  		 System.out.println("현재 잔액은  " + bal );
+			  		 
 				break; //end case 3
-			case 4 : System.out.println("출금 기능입니다.");
+			case 4 : System.out.println("출금 기능입니다. 출금하실 금액을 입력해주세요.");
+					 with = scan.nextInt();
+					 scan.nextLine();
+					 bal -= with;
+					 System.out.println("현재 잔액은  " + bal);
 				break; //end case 4
-			case 5 : System.out.println("삭제합니다.");
+			case 5 : System.out.println("계좌 정보를 삭제합니다. 삭제하시겠습니까? y/n");
+			         ch = scan.next().charAt(0);
+			         scan.nextLine();
+			         if (ch=='y') { id = null;  bal= 0; } 			         
 				break; //end case 5
-		
-		
+				
 				     }
 	
 	
 				     
-				     
+		
 			}
+	      scan.close();	
+
 		}		
 }
 	
